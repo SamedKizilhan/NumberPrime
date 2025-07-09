ea77605bd37bbd37f9e1f616b0ad9658c31efec3
SamedKizilhan <skizilhan35@gmail.com>
2025-07-09 22:49:13 +0300

    added some packages

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello I am Sam. It will be so much fun!</Text>
       <StatusBar style="auto" />
     </View>
   );
