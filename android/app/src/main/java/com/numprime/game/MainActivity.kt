63f814ba9db1defeb2f52a3c0512e62ae980fcf8
SamedKizilhan <skizilhan35@gmail.com>
2025-07-23 18:46:34 +0300

    updated android build files

--- android/app/src/main/java/com/numprime/game/MainActivity.kt
+++ android/app/src/main/java/com/numprime/game/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.NumberPrime
+package com.numprime.game
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/numprime/game/MainApplication.kt
+++ android/app/src/main/java/com/numprime/game/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.NumberPrime
+package com.numprime.game
 
 import android.app.Application
 import android.content.res.Configuration
